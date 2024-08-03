a422e89425a3724fb362dccb12b6ba84a82976df
Isaac Arnault, PhD <44641020+isaacarnault@users.noreply.github.com>
2024-08-03 23:41:09 +0200

    Create I_security_groups.tf

---
+++ I_security_groups.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "sap_s4hana_sg" {
+  name_prefix = "sap-s4hana-sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sap-s4hana-sg"
+  }
+}
+  resource "aws_security_group" "sql_server_sg" {
+  name_prefix = "sql-server-sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 1433
+    to_port     = 1433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sql-server-sg"
+  }
+}
+
